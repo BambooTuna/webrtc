0eb36fb3809110b78398c9fd2a2d2436ff735818
Suzuki Takeo <takeo@stko.info>
2020-12-17 05:10:45 +0900

    wip

--- examples/data-channels/main.go
+++ examples/data-channels/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/pion/webrtc/v3/examples/data-channels/publish"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/pion/webrtc/v3"
 	"github.com/pion/webrtc/v3/examples/internal/signal"
 )
+
+type Offer struct {
+	Sdp string
+}
 
 func main() {
 	// Everything below is the Pion WebRTC API! Thanks for using it ❤️.
@@ -28,115 +35,134 @@ func main() {
 		},
 	}
 
-	// Create a new RTCPeerConnection
-	peerConnection, err := api.NewPeerConnection(config)
-	if err != nil {
-		panic(err)
-	}
-
-	audioTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "audio/opus"}, "audio", "pion1")
-	if err != nil {
-		panic(err)
-	}
-	_, err = peerConnection.AddTrack(audioTrack)
-	if err != nil {
-		panic(err)
-	}
-	_, err = peerConnection.AddTransceiverFromTrack(audioTrack, webrtc.RTPTransceiverInit{Direction: webrtc.RTPTransceiverDirectionSendonly})
-	if err != nil {
-		panic(err)
-	}
-
-	iceConnectedCtx, iceConnectedCtxCancel := context.WithCancel(context.Background())
-	audioStopCtx, audioStop := context.WithCancel(context.Background())
-	go func() {
-		fmt.Println("再生開始")
-		var audio *publish.OggAudio
-		audio, err = publish.NewOggAudio("./output.ogg")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		<-iceConnectedCtx.Done()
-		err = publish.Publisher(audioStopCtx, audioTrack, audio)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println("再生が正常に終わりました")
-	}()
-
-	// Set the handler for ICE connection state
-	// This will notify you when the peer has connected/disconnected
-	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
-		fmt.Printf("ICE Connection State has changed: %s\n", connectionState.String())
-		if connectionState == webrtc.ICEConnectionStateConnected {
-			iceConnectedCtxCancel()
-		} else if connectionState == webrtc.ICEConnectionStateDisconnected {
-			audioStop()
+	route := gin.Default()
+	route.Use(func(ctx *gin.Context) {
+		if origin := ctx.Request.Header.Get("Origin"); origin != "" {
+			ctx.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			if ctx.Request.Method == "OPTIONS" && ctx.Request.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(ctx.Writer, ctx.Request)
+				return
+			}
 		}
 	})
+	route.POST("/sfu", func(c *gin.Context) {
+		var offer Offer
+		_ = c.BindJSON(&offer)
 
-	// Register data channel creation handling
-	peerConnection.OnDataChannel(func(d *webrtc.DataChannel) {
-		fmt.Printf("New DataChannel %s %d\n", d.Label(), d.ID())
+		// Create a new RTCPeerConnection
+		peerConnection, err := api.NewPeerConnection(config)
+		if err != nil {
+			panic(err)
+		}
 
-		// Register channel opening handling
-		d.OnOpen(func() {
-			fmt.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels every 5 seconds\n", d.Label(), d.ID())
+		audioTrack, err := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: "audio/opus"}, "audio", "pion1")
+		if err != nil {
+			panic(err)
+		}
+		_, err = peerConnection.AddTrack(audioTrack)
+		if err != nil {
+			panic(err)
+		}
+		_, err = peerConnection.AddTransceiverFromTrack(audioTrack, webrtc.RTPTransceiverInit{Direction: webrtc.RTPTransceiverDirectionSendonly})
+		if err != nil {
+			panic(err)
+		}
 
-			for range time.NewTicker(5 * time.Second).C {
-				message := signal.RandSeq(15)
-				fmt.Printf("Sending '%s'\n", message)
+		iceConnectedCtx, iceConnectedCtxCancel := context.WithCancel(context.Background())
+		audioStopCtx, audioStop := context.WithCancel(context.Background())
+		go func() {
+			var audio *publish.OggAudio
+			audio, err = publish.NewOggAudio("./output.ogg")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 
-				// Send the message as text
-				sendErr := d.SendText(message)
-				if sendErr != nil {
-					panic(sendErr)
-				}
+			<-iceConnectedCtx.Done()
+			fmt.Println("再生開始")
+			err = publish.Publisher(audioStopCtx, audioTrack, audio)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println("再生が正常に終わりました")
+		}()
+
+		// Set the handler for ICE connection state
+		// This will notify you when the peer has connected/disconnected
+		peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+			fmt.Printf("ICE Connection State has changed: %s\n", connectionState.String())
+			if connectionState == webrtc.ICEConnectionStateConnected {
+				iceConnectedCtxCancel()
+			} else if connectionState == webrtc.ICEConnectionStateDisconnected {
+				audioStop()
 			}
 		})
 
-		// Register text message handling
-		d.OnMessage(func(msg webrtc.DataChannelMessage) {
-			fmt.Printf("Message from DataChannel '%s': '%s'\n", d.Label(), string(msg.Data))
+		// Register data channel creation handling
+		peerConnection.OnDataChannel(func(d *webrtc.DataChannel) {
+			fmt.Printf("New DataChannel %s %d\n", d.Label(), d.ID())
+
+			// Register channel opening handling
+			d.OnOpen(func() {
+				fmt.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels every 5 seconds\n", d.Label(), d.ID())
+
+				for range time.NewTicker(5 * time.Second).C {
+					message := signal.RandSeq(15)
+					fmt.Printf("Sending '%s'\n", message)
+
+					// Send the message as text
+					sendErr := d.SendText(message)
+					if sendErr != nil {
+						panic(sendErr)
+					}
+				}
+			})
+
+			// Register text message handling
+			d.OnMessage(func(msg webrtc.DataChannelMessage) {
+				fmt.Printf("Message from DataChannel '%s': '%s'\n", d.Label(), string(msg.Data))
+			})
 		})
+
+		// Set the remote SessionDescription
+		err = peerConnection.SetRemoteDescription(webrtc.SessionDescription{
+			Type: webrtc.SDPTypeOffer,
+			SDP:  offer.Sdp,
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		// Create an answer
+		answer, err := peerConnection.CreateAnswer(nil)
+		if err != nil {
+			panic(err)
+		}
+
+		// Create channel that is blocked until ICE Gathering is complete
+		gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+
+		// Sets the LocalDescription, and starts our UDP listeners
+		err = peerConnection.SetLocalDescription(answer)
+		if err != nil {
+			panic(err)
+		}
+
+		// Block until ICE Gathering is complete, disabling trickle ICE
+		// we do this because we only can exchange one signaling message
+		// in a production application you should exchange ICE Candidates via OnICECandidate
+		<-gatherComplete
+
+		c.JSON(200, peerConnection.LocalDescription())
 	})
 
-	// Wait for the offer to be pasted
-	offer := webrtc.SessionDescription{}
-	signal.Decode(signal.MustReadStdin(), &offer)
+	_ = route.Run(":8081")
+}
 
-	// Set the remote SessionDescription
-	err = peerConnection.SetRemoteDescription(offer)
-	if err != nil {
-		panic(err)
-	}
-
-	// Create an answer
-	answer, err := peerConnection.CreateAnswer(nil)
-	if err != nil {
-		panic(err)
-	}
-
-	// Create channel that is blocked until ICE Gathering is complete
-	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
-
-	// Sets the LocalDescription, and starts our UDP listeners
-	err = peerConnection.SetLocalDescription(answer)
-	if err != nil {
-		panic(err)
-	}
-
-	// Block until ICE Gathering is complete, disabling trickle ICE
-	// we do this because we only can exchange one signaling message
-	// in a production application you should exchange ICE Candidates via OnICECandidate
-	<-gatherComplete
-
-	// Output the answer in base64 so we can paste it in browser
-	fmt.Println(signal.Encode(*peerConnection.LocalDescription()))
-
-	// Block forever
-	select {}
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept", "Authorization"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
 }
